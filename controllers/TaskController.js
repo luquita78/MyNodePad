77068f964d0dd4d3eb19de83e618194d9958e5c9
luquita78 <lucasbarbosa.oliveira78@gmail.com>
2023-08-29 13:34:27 -0300

    buscar nome via metotdo post

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -1,4 +1,12 @@
-const GetNovaLista = (req,res)=>{
+const GetNovaLista = async (req,res)=>{
+    const NomeLista = req.body;
+    try{
+        await NomeLista
+
+    }catch(err){
+        res.status(500).send({error:err.message})
+    }
+
     res.render("TelaTasks");
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const routes = require("express").Router();
 const {TelaInicial} = require("../controllers/telaInicial")
-const {GetNovaLista} = require("../controllers/TaskController")
+//const {GetNovaLista} = require("../controllers/TaskController")
 
 
 
-routes.get("/inicio",TelaInicial)
-routes.get("/suaLista",GetNovaLista)
+routes.get("/",TelaInicial);
+routes.post("/novaLista/:nome",(req,res)=>{res.render("TelaTasks",()=>res.send(req.params.nome))})
 
 module.exports = routes;
