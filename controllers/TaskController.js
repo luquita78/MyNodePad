3bfc8982a5245c7ebc6de55a10152fa4eca23c8a
luquita78 <lucasbarbosa.oliveira78@gmail.com>
2023-09-05 12:16:44 -0300

    Build: Connection with DB

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -1,13 +1,5 @@
 const GetNovaLista = async (req,res)=>{
-    const NomeLista = req.body;
-    try{
-        await NomeLista
-
-    }catch(err){
-        res.status(500).send({error:err.message})
-    }
-
-    res.render("TelaTasks");
+    
 }
 
 
---
+++ database/db.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+const connectionDb = () =>{
+    mongoose.connect(
+        
+    )
+}
