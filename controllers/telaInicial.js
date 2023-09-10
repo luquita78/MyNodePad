b37f326a2d33d3f6a427b1fbf3fc3a01f5270ac5
luquita78 <lucasbarbosa.oliveira78@gmail.com>
2023-09-10 07:14:18 -0300

    fix: Add itens in the bd

--- controllers/telaInicial.js
+++ controllers/telaInicial.js
@@ -23,29 +23,28 @@ const NovaLista = async (req,res)=>{
     }
 }
 
-const AdicionaItem = async(req,res)=>{
-    const nomeLista = req.params.nomeLista;
-    const nomeItem = req.body.nomeItem;
-
-    ListaSchema.findOne({nome:nomeLista})
-        .then(lista =>{
-            if(!lista){
-                return res.status(404).send({error: 'Lista não encontrada!'});
-            }
-              //Adicionando novo item na lista 
-            lista.itens.push(nomeItem);
-             //salvando item
-            lista.save().then(listaAtualizada =>{
-                return res.status(200).send({message: 'Novo item adicionado!'});
-            }).catch(err=>{
-                return  res.status(500).send({error: 'Erro ao salvar Lista!'});
-            });
-            })
-        .catch(err =>{
-            return res.status(500).send({error:'Erro ao encontrar Lista!'})
-        });
-    
-}
+const AdicionaItem = async (req, res) => {
+    const nomeLista = req.params.nomeLista; // Obtenha o nome da lista a partir dos parâmetros da URL
+    const novoItem = req.body.novoItem; // Supondo que o novo item seja enviado pelo corpo da requisição
+  
+    try {
+      const lista = await ListaSchema.findOne({ nome: nomeLista }); // Encontre a lista pelo nome
+  
+      if (!lista) {
+        return res.status(404).send({ error: 'Lista não encontrada.' });
+      }else{
+  
+      lista.itens.push({ valor: novoItem }); // Adicione o novo item à lista
+      await lista.save(); // Salve a lista atualizada
+  
+      res.status(200).send({ message: 'Novo item adicionado com sucesso!' });}
+      
+    } catch (err) {
+      console.error('Erro ao adicionar item à lista:', err);
+      res.status(500).send({ error: 'Erro ao adicionar item à lista.' });
+    }
+  }
+  
 
 const LancaNovaLista = (req,res)=>{
     const nomeLista = req.params.nomeLista;
--- routes/index.js
+++ routes/index.js
@@ -5,28 +5,6 @@ const ListaSchema = require("../models/Task")
 routes.get("/",TelaInicial);
 routes.post("/novaLista", NovaLista);
 routes.get('/:nomeLista',LancaNovaLista);
-routes.post('/createTask/:nomeLista',async (req, res) => {
-    const nomeLista = req.params.nomeLista; // Obtenha o nome da lista a partir dos parâmetros da URL
-    const novoItem = req.body.novoItem; // Supondo que o novo item seja enviado pelo corpo da requisição
-  
-    try {
-      const lista = await ListaSchema.findOne({ nome: nomeLista }); // Encontre a lista pelo nome
-  
-      if (!lista) {
-        return res.status(404).send({ error: 'Lista não encontrada.' });
-      }
-  
-      lista.itens.push({ valor: novoItem }); // Adicione o novo item à lista
-      await lista.save(); // Salve a lista atualizada
-  
-      res.status(200).send({ message: 'Novo item adicionado com sucesso!' });
-      
-    } catch (err) {
-      console.error('Erro ao adicionar item à lista:', err);
-      res.status(500).send({ error: 'Erro ao adicionar item à lista.' });
-    }
-  }
-  
-  );
+routes.post('/createTask/:nomeLista',AdicionaItem);
 
 module.exports = routes;
