e559d8b9f1d07b4555fa880656ff5fe25069f3c9
luquita78 <lucasbarbosa.oliveira78@gmail.com>
2023-09-24 16:55:43 -0300

    fix: update item

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const routes = require("express").Router();
-const {TelaInicial, NovaLista, LancaNovaLista,AdicionaItem, GetItensById} = require("../controllers/index")
+const {TelaInicial, NovaLista, LancaNovaLista,AdicionaItem, GetItensById,UpdateOne} = require("../controllers/index");
+const { updateOne } = require("../models/Task");
 
 routes.get("/",TelaInicial);
-routes.post("/novaLista", NovaLista);
 routes.get('/:nomeLista',LancaNovaLista);
 routes.get('/:nomeLista/:itemId/:method', GetItensById);
+
+routes.post("/novaLista", NovaLista);
 routes.post('/createTask/:nomeLista',AdicionaItem);
+routes.post('/update/:nomeLista/:itemId', updateOne);
 
 
 
